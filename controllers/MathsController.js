eaf0d09fee83b784e18cc689110cf302d5269a93
Mathieu007657 <mathieulavallee007657@gmail.com>
2024-10-06 11:09:00 -0400

    modification du controlleur et index qui avance le  focntionnement

--- controllers/MathsController.js
+++ controllers/MathsController.js
@@ -5,106 +5,101 @@ export default class MathsController extends Controller {
   constructor(HttpContext) {
     super(HttpContext);
   }
+
   get(id) {
-    let x = parseInt(this.HttpContext.path.params.x);
-    let y = parseInt(this.HttpContext.path.params.y);
-    let n = parseInt(this.HttpContext.path.params.n);
-    var response = {};
+    let response = {};
+    let x = this.HttpContext.path.params.x !== undefined ? parseInt(this.HttpContext.path.params.x) : undefined;
+    let y = this.HttpContext.path.params.y !== undefined ? parseInt(this.HttpContext.path.params.y) : undefined;
+    let n = this.HttpContext.path.params.n !== undefined ? parseInt(this.HttpContext.path.params.n) : undefined;
+    let op = this.HttpContext.path.params.op;
     
-    response.op = this.HttpContext.path.params.op;
-    switch (this.HttpContext.path.params.op) {
-      case " ":
+    if (!op) {
+      response.error = "Operation 'op' is missing";
+      return this.HttpContext.response.JSON(response, 422);
+    }
+    
+    if ((op === "+" || op === "-" || op === "*" || op === "/" || op === "%") && (x === undefined || y === undefined)) {
+      response.error = "Parameters 'x' and 'y' are required for the operation";
+      return this.HttpContext.response.JSON(response, 422);
+    }
+
+    if ((op === "!" || op === "p" || op === "np") && n === undefined) {
+      response.error = "Parameter 'n' is required for this operation";
+      return this.HttpContext.response.JSON(response, 422);
+    }
+
+    if (op === "/" && y === 0) {
+      response.error = "'y' parameter cannot be zero for division";
+      return this.HttpContext.response.JSON(response, 422);
+    }
+    
+
+    if ((op === "+" || op === "-" || op === "*" || op === "/" || op === "%") && (isNaN(x) || isNaN(y))) {
+      response.error = `'${isNaN(x) ? "x" : "y"}' parameter is not a number`;
+      return this.HttpContext.response.JSON(response, 422);
+    }
+
+    if ((op === "!" || op === "p" || op === "np") && isNaN(n)) {
+      response.error = "'n' parameter is not a number";
+      return this.HttpContext.response.JSON(response, 422);
+    }
+    
+    response.op = op;
+    switch (op) {
+      case "+":
         response.x = x;
         response.y = y;
-        if (!isNaN(x) && !isNaN(y)) {
-          response.value = x + y;
-        } else if (!isNaN(x)) {
-          response.error = "x is not a number";
-        } else if (!isNaN(y)) {
-          response.error = "y is not a number";
-        }
+        response.value = x + y;
         break;
       case "-":
         response.x = x;
         response.y = y;
-        if (!isNaN(x) && !isNaN(y)) {
-          response.value = x - y;
-        } else if (!isNaN(x)) {
-          response.error = "x is not a number";
-        } else if (!isNaN(y)) {
-          response.error = "y is not a number";
-        }
+        response.value = x - y;
         break;
       case "*":
         response.x = x;
         response.y = y;
-        if (!isNaN(x) && !isNaN(y)) {
-          response.value = x * y;
-        } else if (!isNaN(x)) {
-          response.error = "x is not a number";
-        } else if (!isNaN(y)) {
-          response.error = "y is not a number";
-        }
+        response.value = x * y;
         break;
       case "/":
         response.x = x;
         response.y = y;
-        if (!isNaN(x) && !isNaN(y)) {
-          response.value = x / y;
-        } else if (!isNaN(x)) {
-          response.error = "x is not a number";
-        } else if (!isNaN(y)) {
-          response.error = "y is not a number";
-        }
+        response.value = x / y;
         break;
       case "%":
         response.x = x;
         response.y = y;
-        if (!isNaN(x) && !isNaN(y)) {
-          response.value = x % y;
-        } else if (!isNaN(x)) {
-          response.error = "x is not a number";
-        } else if (!isNaN(y)) {
-          response.error = "y is not a number";
-        }
+        response.value = x % y;
         break;
       case "!":
         response.n = n;
-        if (!isNaN(n)) {
-          response.value = mathUtilities.factorial(n);
-        } else {
-          response.error = "n is not a number";
-        }
+        response.value = mathUtilities.factorial(n);
         break;
       case "p":
         response.n = n;
-        if (!isNaN(n)) {
-          response.value = mathUtilities.isPrime(n);
-        } else {
-          response.error = "n is not a number";
-        }
+        response.value = mathUtilities.isPrime(n);
         break;
       case "np":
         response.n = n;
-        if (!isNaN(n)) {
-          response.value = mathUtilities.findPrime(n);
-        } else {
-          response.error = "n is not a number";
-        }
+        response.value = mathUtilities.findPrime(n);
         break;
-        default:
-            response.error = "Unknown operation";
-        break;
+      default:
+        response.error = "Unknown operation";
+        return this.HttpContext.response.JSON(response, 422);
     }
+
     this.HttpContext.response.JSON(response);
   }
+
   post(data) {
-    this.HttpContext.response.JSON("Unvalid Operation");
+    this.HttpContext.response.JSON({ error: "POST is not allowed for this endpoint" }, 405);
   }
+
   put(data) {
-    this.HttpContext.response.JSON("Unvalid Operation");
+    this.HttpContext.response.JSON({ error: "PUT is not allowed for this endpoint" }, 405);
   }
+
   remove(id) {
-    this.HttpContext.response.JSON("Unvalid Operation");
+    this.HttpContext.response.JSON({ error: "DELETE is not allowed for this endpoint" }, 405);
   }
 }
