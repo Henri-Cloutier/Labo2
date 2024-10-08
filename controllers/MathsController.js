535f0110a28c2405e50e987e296241f22c2f8a71
Henri-Cloutier <hcloutier05@gmail.com>
2024-10-07 21:37:16 -0400

    Fix de base

--- controllers/MathsController.js
+++ controllers/MathsController.js
@@ -8,40 +8,40 @@ export default class MathsController extends Controller {
 
   get(id) {
     let response = {};
-    let x = this.HttpContext.path.params.x !== undefined ? parseInt(this.HttpContext.path.params.x) : undefined;
-    let y = this.HttpContext.path.params.y !== undefined ? parseInt(this.HttpContext.path.params.y) : undefined;
-    let n = this.HttpContext.path.params.n !== undefined ? parseInt(this.HttpContext.path.params.n) : undefined;
+    let x = this.HttpContext.path.params.x !== undefined ? parseFloat(this.HttpContext.path.params.x) : undefined;
+    let y = this.HttpContext.path.params.y !== undefined ? parseFloat(this.HttpContext.path.params.y) : undefined;
+    let n = this.HttpContext.path.params.n !== undefined ? parseFloat(this.HttpContext.path.params.n) : undefined;
     let op = this.HttpContext.path.params.op;
     
     if (!op) {
       response.error = "Operation 'op' is missing";
-      return this.HttpContext.response.JSON(response, 422);
+      return this.HttpContext.response.JSON(response, 200);
     }
     
     if ((op === "+" || op === "-" || op === "*" || op === "/" || op === "%") && (x === undefined || y === undefined)) {
       response.error = "Parameters 'x' and 'y' are required for the operation";
-      return this.HttpContext.response.JSON(response, 422);
+      return this.HttpContext.response.JSON(response, 200);
     }
 
     if ((op === "!" || op === "p" || op === "np") && n === undefined) {
       response.error = "Parameter 'n' is required for this operation";
-      return this.HttpContext.response.JSON(response, 422);
+      return this.HttpContext.response.JSON(response, 200);
     }
 
     if (op === "/" && y === 0) {
       response.error = "'y' parameter cannot be zero for division";
-      return this.HttpContext.response.JSON(response, 422);
+      return this.HttpContext.response.JSON(response, 200);
     }
     
 
     if ((op === "+" || op === "-" || op === "*" || op === "/" || op === "%") && (isNaN(x) || isNaN(y))) {
       response.error = `'${isNaN(x) ? "x" : "y"}' parameter is not a number`;
-      return this.HttpContext.response.JSON(response, 422);
+      return this.HttpContext.response.JSON(response, 200);
     }
 
     if ((op === "!" || op === "p" || op === "np") && isNaN(n)) {
       response.error = "'n' parameter is not a number";
-      return this.HttpContext.response.JSON(response, 422);
+      return this.HttpContext.response.JSON(response, 200);
     }
     
     response.op = op;
@@ -85,21 +85,21 @@ export default class MathsController extends Controller {
         break;
       default:
         response.error = "Unknown operation";
-        return this.HttpContext.response.JSON(response, 422);
+        return this.HttpContext.response.JSON(response, 200);
     }
 
     this.HttpContext.response.JSON(response);
   }
 
   post(data) {
-    this.HttpContext.response.JSON({ error: "POST is not allowed for this endpoint" }, 405);
+    this.HttpContext.response.JSON({ error: "POST is not allowed for this endpoint" }, 200);
   }
 
   put(data) {
-    this.HttpContext.response.JSON({ error: "PUT is not allowed for this endpoint" }, 405);
+    this.HttpContext.response.JSON({ error: "PUT is not allowed for this endpoint" }, 200);
   }
 
   remove(id) {
-    this.HttpContext.response.JSON({ error: "DELETE is not allowed for this endpoint" }, 405);
+    this.HttpContext.response.JSON({ error: "DELETE is not allowed for this endpoint" }, 200);
   }
 }
